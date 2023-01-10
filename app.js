0f8af3cee1922786e26ced4661bffba6d69d1b4a
Marcos Arias <johndoe@example.com>
2023-01-10 12:21:35 -0300

    controlador de creacion de usuarios funcionando , solo falta la conexion con la base de datos

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+require('./database/db');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
+const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
-
+const userRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+mongoose.set('strictQuery', true);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
+app.use('/users', userRouter);
+mongoose.set('strictQuery', true);
 
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const { createUser } = require('../controller/users');
+
 router
-  .post('/', createUser);
+  .post('', createUser);
+
+module.exports = router;
